ad1aa5deec3853a068ac9a88c0c69b9759b057ce
Henrik Hannemose <1955579+henrikhannemose@users.noreply.github.com>
2019-05-14 09:39:59 +0200

    Removed deprecated ILMerge tooling and replaced with newer version. Updated tool versions

--- XrmOrg/XrmOrg.XrmSolution/Tests/Metadata/TypeDeclarations.cs
+++ XrmOrg/XrmOrg.XrmSolution/Tests/Metadata/TypeDeclarations.cs
@@ -1,39 +1,6 @@
 using System;
 
 namespace DG.Tools.XrmMockup {
-	public struct SecurityRoles {
-		public static Guid _000TestingRole = new Guid("098d1877-3ee9-e611-80ef-c4346bacfc18");
-		public static Guid ActivityFeeds = new Guid("cb269f84-e19d-e011-b66c-00155db528b6");
-		public static Guid AnotherName = new Guid("5a340290-d69b-e811-a84a-000d3a2a0765");
-		public static Guid Cannotcreatecontact = new Guid("431e71c1-e107-e711-80db-3863bb35ff48");
-		public static Guid Cannotreadcontact = new Guid("f3815504-e25c-e711-80fa-c4346bac0e68");
-		public static Guid CEOBusinessManager = new Guid("fe435cab-c047-e611-80d9-c4346badf080");
-		public static Guid CommonDataServiceUser = new Guid("0ed47014-0970-e811-a849-000d3a2a925d");
-		public static Guid createRef = new Guid("4b82b382-59b7-e811-a845-000d3a2a0687");
-		public static Guid CSRManager = new Guid("29525cab-c047-e611-80d9-c4346badf080");
-		public static Guid Customerserviceappaccess = new Guid("3dac4d9e-568c-e611-80d4-00155d42a122");
-		public static Guid CustomerServiceRepresentative = new Guid("7a355cab-c047-e611-80d9-c4346badf080");
-		public static Guid Delegate = new Guid("963d5cab-c047-e611-80d9-c4346badf080");
-		public static Guid Differentname = new Guid("6b9d42f7-d038-e911-a84e-000d3a2a0687");
-		public static Guid EniroUser = new Guid("f5b22a2e-214b-e511-80dd-c4346badb0ac");
-		public static Guid EnvironmentMaker = new Guid("d58407f2-48d5-e711-a82c-000d3a37c848");
-		public static Guid KnowledgeManager = new Guid("a94b5cab-c047-e611-80d9-c4346badf080");
-		public static Guid MarketingManager = new Guid("50475cab-c047-e611-80d9-c4346badf080");
-		public static Guid MarketingProfessional = new Guid("3e405cab-c047-e611-80d9-c4346badf080");
-		public static Guid PlaybookManager = new Guid("4178df9e-4ea5-e811-8149-000d3a3a2732");
-		public static Guid PlaybookUser = new Guid("4178df9e-4ea5-e811-8149-000d3a3a2731");
-		public static Guid RelationshipInsightsAdmin = new Guid("016fd16e-7be7-e611-80d6-00155d204901");
-		public static Guid SalesManager = new Guid("cb3d5cab-c047-e611-80d9-c4346badf080");
-		public static Guid Salesperson = new Guid("b6495cab-c047-e611-80d9-c4346badf080");
-		public static Guid Samename = new Guid("356fd55d-d69b-e811-a84a-000d3a2a0765");
-		public static Guid ScheduleManager = new Guid("32425cab-c047-e611-80d9-c4346badf080");
-		public static Guid Scheduler = new Guid("82505cab-c047-e611-80d9-c4346badf080");
-		public static Guid SupportUser = new Guid("95465cab-c047-e611-80d9-c4346badf080");
-		public static Guid SurveyOwner = new Guid("f9a5a958-d07f-44cd-886d-eb6343309c2a");
-		public static Guid SurveyServicesAdministrator = new Guid("abce3b01-5697-4973-9d7d-fca48ca84445");
-		public static Guid SystemAdministrator = new Guid("ee395cab-c047-e611-80d9-c4346badf080");
-		public static Guid SystemCustomizer = new Guid("a54d5cab-c047-e611-80d9-c4346badf080");
-		public static Guid VicePresidentofMarketing = new Guid("7c375cab-c047-e611-80d9-c4346badf080");
-		public static Guid VicePresidentofSales = new Guid("5edd53b1-c047-e611-80d9-c4346badf080");
-	}
+    public struct SecurityRoles {
+    }
 }
